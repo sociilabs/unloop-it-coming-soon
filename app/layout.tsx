a79b5e200bb31fa1eba05251e14ca8e153692055
Muhammad Arslan Aslam <hello@arslanaslam.me>
2025-06-23 06:14:12 +0500

    update application name and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Unloop IT',
+  description: 'Your trusted partner in navigating the complexities of AI, automation, and modern IT strategy.',
+  generator: 'Unloop IT',
 }
 
 export default function RootLayout({
